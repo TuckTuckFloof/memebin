0af40ce1bc88d926edb46b2898c02270835b0989
floof <floof@pop-os.localdomain>
2020-09-28 18:35:48 -0700

    I have no idea what I did last commit but I have the Inventory class done as well as the ability to create a class object

--- TakingInv/inventory.py
+++ TakingInv/inventory.py
@@ -1,14 +1,43 @@
-# This is a program that makes what the bone heads at my job couldn't do, a basic inventory system
-
 class Inventory:
-    def __init__(self, department, category, style, price):
+    def __init__(self, department, category, style, price, desc):
         self.department = department
         self.category = category
         self.style = style
         self.price = price
+        self.desc = desc
+
+    def get_department(self):
+        print(self.department)
+    
+    def get_category(self):
+        print(self.category)
+
+    def get_style(self):
+        print(self.style)
+
+    def get_price(self):
+        print(self.price)
+
+    def get_desc(self):
+        print(self.desc)
+
+    def set_department(self, department):
+        self.department = department
+
+    def set_category(self, category):
+        self.category = category
+
+    def set_style(self, style):
+        self.style = style
+
+    def set_price(self, price):
+        self.price = price
+
+    def set_desc(self, desc):
+        self.desc = desc
 
     def credit_card_price(self):
         return "10% TJX Credit Card Coupon Price: ${0:.3g}".format(self.price * (.9))
 
     def display_info(self):
-        print("Dept: {} \nCatg: {}\nStyle: {}\nBase Price: ${}".format(self.department, self.category, self.style, self.price))
+        print("Dept: {} \nCatg: {}\nStyle: {}\nBase Price: ${}\nDescription: {}\n".format(self.department, self.category, self.style, self.price, self.desc))
--- TakingInv/main.py
+++ TakingInv/main.py
@@ -1,6 +1,44 @@
 from inventory import Inventory
+from os import system
 
-mug = Inventory(56, 2020, 123456, 3.99)
+def main():
+    answer = -1 
+    while answer != 0:
+        display_options()
+        try:
+            answer = int(input('Insert a number: '))
+            system('clear')
+        except ValueError:
+            system('clear')
+            print('Not a valid input\n')
 
-print(mug.credit_card_price())
-mug.display_info()
+        if answer == 1:
+            add_item()
+
+def display_options():
+    print('''\nWhat would you like to do?
+    1) Add a new item
+    2) Adjust aspects for item
+    3) Search for an item
+    4) Delete an item
+    0) Exit the Program\n''')
+
+# Should create a new instance of the class
+def add_item():
+    try:
+        item_department = int(input("Please enter department number:\n"))
+        item_category = int(input("Please enter category number:\n"))
+        item_style = int(input("Please enter 6-digit style number:\n"))
+        item_price = float(input("Please enter the price of the item:\n$"))
+        item_desc = input("Please enter a brief description of the item:\n")
+    except ValueError:
+        system('clear')
+        print('Not a valid input\n')
+        add_item()
+    
+    # This is where it adds the item to the stack but we haven't gotten that far yet so we're just going to display the class itself lmaooooo
+    system('clear')
+    item = Inventory(item_department, item_category, item_style, item_price, item_desc)
+    item.display_info()
+
+main()
